3e52bffa1e1e0ee1298c52bfdf97d90c433b494f
Michele Bisceglia <mikael_bisceglia@libero.it>
2022-05-17 17:14:09 +0200

    header structure

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <div>
+      <div class="logo-container"></div>
+    </div>
+    <div class="header-list">
+      <ul></ul>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<style lang="scss">
+.logo-container {
+  height: 100px;
+  width: 100px;
+  background-color: yellowgreen;
+  float: left;
+  margin-left: 100px;
+}
+.header-list {
+  height: 100px;
+  width: 300px;
+  background-color: red;
+  float: right;
+  margin-right: 100px;
+}
+</style>
